99c5112eba6265e1e00074bfb0d8942fc66c86f9
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-18 19:09:50 +0100

    Added test to value inputs '1,1'

--- app.test.js
+++ app.test.js
@@ -7,3 +7,19 @@ const strictEquals = (a, b) => {
     return false;
   }
 };
+
+// ----- TESTING ---------------------
+
+describe("Given a strictEquals function", () => {
+  describe("When it receives two identical numbers", () => {
+    test("Then it should return true", () => {
+      // Arrange
+      const number = 1;
+      const expectedResult = true;
+      // Act
+      const result = strictEquals(number, number);
+      // Assert
+      expect(result).toBe(expectedResult);
+    });
+  });
+});
